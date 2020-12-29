4112bdce85cf59364350d644150657e16082e490
Pawel Maryon <maryonpawel@gmail.com>
2020-12-29 09:20:25 +0000

    destroy action done

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,7 +19,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to @portfolio_item, notice: 'Blog was successfully updated.' }
+        format.html { redirect_to @portfolio_item, notice: 'Portfolio item was successfully updated.' }
         format.json { render :show, status: :ok, location: @portfolio_item }
       else
         format.html { render :edit }
@@ -32,12 +32,21 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new(portfolio_params)
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'portfolio_item was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
         format.json { render :show, status: :created, location: @portfolio_item }
       else
         format.html { render :new }
         format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item was removed.' }
     end
   end
 
