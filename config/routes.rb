3653b14ba34a3d5a79ea997f0918eaad739011d1
Pawel Maryon <maryonpawel@gmail.com>
2020-12-29 12:11:55 +0000

    post status update done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   resources :portfolios, except: [:show]
 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
@@ -7,7 +6,10 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-
-  resources :blogs
+  resources :blogs do
+    member do
+      get 'toggle_status'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
