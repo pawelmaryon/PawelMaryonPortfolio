81c9398c980f353930c8a783c1978a31cc62d217
Pawel Maryon <maryonpawel@gmail.com>
2020-12-30 11:13:37 +0000

    implemented concern for managing data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,13 +1,15 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates :title, :body, :main_image, :thumb_image, presence: true
   def self.react
     where(subtitle: 'React')
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
   after_initialize :set_defaults
   def set_defaults
-    self.main_image ||= 'http://placehold.it/600x400'
-    self.thumb_image ||= 'http://placehold.it/350x200'
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
