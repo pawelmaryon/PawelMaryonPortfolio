99c853fdb1e1c716bd618cecd570a87212f5a080
Pawel Maryon <maryonpawel@gmail.com>
2021-01-02 12:01:45 +0000

    portfolio new form partial implemented

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,17 +25,16 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
+    3.times { @portfolio_item.technologies.build }
   end
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to @portfolio_item, notice: 'Portfolio item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @portfolio_item }
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
       else
         format.html { render :edit }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
   end
