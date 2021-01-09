e8c05d1f1acb8b1bb9e4e477d699581952a413ea
Pawel Maryon <maryonpawel@gmail.com>
2021-01-09 09:53:23 +0000

    layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+  include PMaryonViewTool
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = PMaryonViewTool::Renderer.copyright('Pawel Maryon', 'All Rights Reserved', 'b')
+  end
 end
