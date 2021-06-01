1d213131b309343cdb8bed4e4f96d0929ca4bc70
Pawel Maryon <maryonpawel@gmail.com>
2021-06-01 22:17:22 +0100

    coffe script implementations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery-ujs
 //= require jquery-ui
+//= require html.sortable
+//= require turbolinks
+//= require_tree .
+
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
